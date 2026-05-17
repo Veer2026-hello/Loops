e940e4752e379e872a7ed25ac055960bdaaf9cf2
Veer2026-hello <jagrutinandola@gmail.com>
2026-05-17 14:13:54 +0100

    Add files via upload

---
+++ Activity 3.py
@@ -0,0 +1,10 @@
+# Python program to calculate the sum of the first ten natural using a while loop
+
+total_sum = 0
+num = 1
+
+while num <= 10:
+    total_sum += num
+    num += 1
+
+print(f"The sum of the first ten natural number is {total_sum}")
